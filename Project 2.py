544f236e02047191af02a60650d3cbd230e7cd32
Ananya161113 <ananyap.1611@gmail.com>
2024-09-17 14:18:10 +0530

    Add files via upload

---
+++ Project 2.py
@@ -0,0 +1,14 @@
+# H.W
+Password= "Nature"
+Userpassword =input("Enter your password :")
+while Userpassword!=Password:
+   print("Wrong password")
+   Userpassword =input("Enter your password :")
+print("Correct password")
+
+
+
+
+
+
+
